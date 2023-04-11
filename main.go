c306e378467b0b442bf7cbb146786ac25c95ad07
Rio Gosal <rio@lucys.app>
2023-04-11 13:35:06 +0800

    Add recover during panic and increased 100kb buff

--- main.go
+++ main.go
@@ -19,9 +19,15 @@ func main() {
 
 	log_file, err := os.OpenFile(fmt.Sprintf("%s/nautilus-print-server.log", os.TempDir()), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
 	if err != nil {
-		log.Default().Fatal(err)
+		log.Default().Panic(err)
 	}
 	defer log_file.Close()
+	defer func() {
+		if r := recover(); r != nil {
+			time.Sleep(1 * time.Second)
+			log.Default().Printf("Recovered from opening log file: %s", r)
+		}
+	}()
 	log.Initialize(log_file)
 
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
